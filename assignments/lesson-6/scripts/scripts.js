d6ccd73e9fd77ad7667719835f1c05477820fdfa
ajorgensen5 <ajorgensen5@msn.com>
2018-02-18 10:13:09 -0700

    updates to

--- assignments/lesson-6/scripts/scripts.js
+++ assignments/lesson-6/scripts/scripts.js
@@ -1,2 +1,13 @@
 function toggleMenu() {
-    console.log(get.documentElementById("sally").classList);
+    console.log("Test");
+
+    if (document.getElementById("navigation").style.display == "inherit")
+
+}
+
+var d = new Date();
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+document.getElementById("demo").innerHTML = currentdate[d.getDay()];
+
+document.getElementById("demo").innerHTML = d.getFullYear();
+
