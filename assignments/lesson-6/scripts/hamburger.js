e1b5eae4e267f83d691a7d141221a5a84db3e7dc
ajorgensen5 <ajorgensen5@msn.com>
2018-02-22 16:20:55 -0700

    fix hamburger bar

--- assignments/lesson-6/scripts/hamburger.js
+++ assignments/lesson-6/scripts/hamburger.js
@@ -1,3 +1,14 @@
 function toggleHam() {
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+    var x = document.getElementById("nav");
+    if (x.classList == "show-nav") {
+        x.classList.remove("show-nav");
+        x.classList.add("hide-nav");
+    }
+    
+    else if (x.classList=="hide-nav")
+    {
+       x.classList.remove("hide-nav");
+        x.classList.add("show-nav"); 
+    }
+
 }
