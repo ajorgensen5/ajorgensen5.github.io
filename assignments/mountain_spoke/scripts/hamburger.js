7892d8be3b4b78e8b94ce036df8a5e238aadb92d
ajorgensen5 <ajorgensen5@msn.com>
2018-04-11 19:26:32 -0600

    Toggle Hamburger Fix

--- assignments/mountain_spoke/scripts/hamburger.js
+++ assignments/mountain_spoke/scripts/hamburger.js
@@ -1,35 +1,4 @@
 function toggleHam() {
-    var x = document.getElementById("nav");
-    if (x.classList == "show-nav") {
-        x.classList.remove("show-nav");
-        x.classList.add("hide-nav");
-    }
-    
-    else if (x.classList=="hide-nav")
-    {
-       x.classList.remove("hide-nav");
-        x.classList.add("show-nav"); 
-    }
-
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
-
- //remember to add an ID of ?mainmenu" to the nav ul"
-            var urlString = document.location.href;
-            varurlArray = urlString.split('/');
-            var pageHREF = urlArray[urlArray.length - 1];
-
-            if (pageHREF !== "") {
-                var menu = document.querySelectorAll('ul#mainmenu li a');
-                var i;
-                //loop through all the menu items in the navigation
-                for (i = 0; i < menu.length; i++) {
-                    var currentURL = (menu[i].getAttribute("href"));
-                    menu[i].parentNode.className = ""; //turn off highlighting by default
-                    if (currentURL === pageHREF) {
-                        menu[i] parentNode.className = "active";
-                    } //end if
-                } //end of the search for a match
-            } //end of if !==
-
-
 
